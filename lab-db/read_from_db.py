997b8cbb61ada5a1e3e405655665b722aea10c52
axlpierola <axlpierola@gmail.com>
2023-08-24 22:12:59 -0500

    update lab rds

--- lab-db/read_from_db.py
+++ lab-db/read_from_db.py
@@ -1,17 +1,19 @@
+# read_from_db.py
+
 import pymysql
 
 def fetch_values():
     # Endpoints y configuración
-    reader_endpoint = 'xxxxxxxxxxxxxxx.us-east-1.rds.amazonaws.com'  # Aquí el endpoint del nodo de lectura
-    db_name = 'example'
-    user = 'example'
-    password = 'example'
+    reader_endpoint = 'demo.cluster-c8ngcue4bxze.us-east-1.rds.amazonaws.com'  # Aquí el endpoint del nodo de lectura
+    db_name = 'ragnar'
+    user = 'admin'
+    password = 'DJVQOB6NdfL8ulTguklP'
 
     # Conexión al nodo de lectura y lectura
     connection = pymysql.connect(host=reader_endpoint, user=user, password=password, database=db_name)
     try:
         with connection.cursor() as cursor:
-            # NOTA: Cambié  `ragnar` por `su-tabla`  para coincidir con el nmbre de la tabla.
+            # NOTA: Cambié `data` por `value` y `test_table` por `ragnar` para coincidir con la estructura de la tabla.
             sql = "SELECT `value` FROM `ragnar`"
             cursor.execute(sql)
             results = cursor.fetchall()
--- lab-db/write_to_db.py
+++ lab-db/write_to_db.py
@@ -1,32 +1,29 @@
-# Módulo pymysql requerido, instalar con pip:
-# pip3 install pymysql
+# write_to_db.py
 
 import pymysql
 import uuid
 
 def write_random_value():
+    # Endpoints y configuración
+    writer_endpoint = 'demo.cluster-c8ngcue4bxze.us-east-1.rds.amazonaws.com'
+    db_name = 'ragnar'
+    user = 'admin'
+    password = 'DJVQOB6NdfL8ulTguklP'
+
+    # Generar un valor aleatorio UUID
+    random_value = str(uuid.uuid4())
+
+    # Conexión al nodo principal y escritura
+    connection = pymysql.connect(host=writer_endpoint, user=user, password=password, database=db_name)
     try:
-        print("Intentando escribir en la base de datos...")
-        
-        # Endpoints y configuración
-        writer_endpoint = 'xxxxxxxxxxxxxxx.us-east-1.rds.amazonaws.com'
-        db_name = 'example'
-        user = 'example'
-        password = 'example'
-
-        # Generar un valor aleatorio UUID
-        random_value = str(uuid.uuid4())
-
-        # Conexión al nodo principal y escritura
-        connection = pymysql.connect(host=writer_endpoint, user=user, password=password, database=db_name)
         with connection.cursor() as cursor:
+            # NOTA: Cambié `data` por `value` para coincidir con la estructura de la tabla.
             sql = "INSERT INTO `ragnar` (`value`) VALUES (%s)"
             cursor.execute(sql, (random_value,))
         connection.commit()
         print(f"Valor {random_value} insertado con éxito.")
-
-    except Exception as e:
-        print(f"Error durante la inserción: {e}")
     finally:
-        if 'connection' in locals():
-            connection.close()
+        connection.close()
+
+if __name__ == "__main__":
+    write_random_value()

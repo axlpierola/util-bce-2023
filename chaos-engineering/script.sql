48e420c4c817d704fada43e1c850c4e50dc88d84
axlpierola <axlpierola@gmail.com>
2023-09-14 18:40:33 -0500

    chaos eng

---
+++ chaos-engineering/script.sql
@@ -0,0 +1,4 @@
+USE chaos;
+UPDATE messages 
+SET message_text = 'Has superado el reto de Chaos Engineering, estás listo para ir al mundo real!!' 
+WHERE message_text = 'Estás próximo a superar el reto de Chaos Engineering, actualiza este mensaje con el script compartido!! ';
